189a68c08e64da7b242d8551ff7a997e4c2611c4
Yuyang Lee <i@aidenli.net>
2023-10-20 15:40:22 +0800

    use relative path in run scripts

--- run_rgbd.sh
+++ run_rgbd.sh
@@ -1,10 +1,10 @@
-Vocabulary=/home/yuyang/dev/SLAM/ORB_SLAM3/Vocabulary/ORBvoc.txt
-Setting=/home/yuyang/dev/SLAM/ORB_SLAM3/Examples/RGB-D/rgbd_sim.yaml
-DATA=/home/yuyang/dev/SLAM/dataset/rosbag2_2023_10_19-20_18_12
+Vocabulary=./Vocabulary/ORBvoc.txt
+Setting=./Examples/RGB-D/rgbd_sim.yaml
+DATA=../dataset/rosbag2_2023_10_19-20_18_12
 ImageAssociation=${DATA}/image_filelist.txt
-EXEC=/home/yuyang/dev/SLAM/ORB_SLAM3/Examples/RGB-D/rgbd_sim
+EXEC=./Examples/RGB-D/rgbd_sim
 
-RESULT=/home/yuyang/dev/SLAM/ORB_SLAM3/tf_pred.txt
+RESULT=./tf_pred.txt
 
 ${EXEC} ${Vocabulary} ${Setting} ${ImageAssociation}
 
--- run_rgbd_inertial.sh
+++ run_rgbd_inertial.sh
@@ -1,9 +1,9 @@
-Vocabulary=/home/yuyang/dev/SLAM/ORB_SLAM3/Vocabulary/ORBvoc.txt
-Setting=/home/yuyang/dev/SLAM/ORB_SLAM3/Examples/RGB-D-Inertial/rgbd_inertial_sim.yaml
-DATA=/home/yuyang/dev/SLAM/dataset/rosbag2_2023_10_13-17_18_05
+Vocabulary=./Vocabulary/ORBvoc.txt
+Setting=./Examples/RGB-D-Inertial/rgbd_inertial_sim.yaml
+DATA=../dataset/rosbag2_2023_10_13-17_18_05
 ImageAssociation=${DATA}/image_filelist.txt
 ImuAssociation=${DATA}/imu_filelist.txt
 
-EXEC=/home/yuyang/dev/SLAM/ORB_SLAM3/Examples/RGB-D-Inertial/rgbd_inertial_sim
+EXEC=./Examples/RGB-D-Inertial/rgbd_inertial_sim
 
 ${EXEC} ${Vocabulary} ${Setting} ${ImageAssociation} ${ImuAssociation}
